d05fb4c12bb162ed427506c07bd018e910fdb4b0
Jean-Daniel de Ambrogi <58121252+JD-DA@users.noreply.github.com>
2022-04-14 11:58:26 +0200

    color shading

--- app/src/main/java/com/example/air_hockey/Air_Hockey_Renderer.java
+++ app/src/main/java/com/example/air_hockey/Air_Hockey_Renderer.java
@@ -44,37 +44,43 @@ public class Air_Hockey_Renderer implements GLSurfaceView.Renderer {
     private final Context context;
     private int program;
 
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
 
     private static final String A_POSITION = "a_Position";
     private int aPositionLocation;
 
+    private static final String A_COLOR = "a_Color";
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    private int aColorLocation;
+
+
     public Air_Hockey_Renderer(Context context) {
         float[] tableVertices = {
 //triangle 1
-                -0.5f, -0.5f,
-                0.5f,  0.5f,
-                -0.5f,  0.5f,
+                -0.5f, -0.5f,0.4f, 0.4f, 0.4f,
+                0.5f,  0.5f,0.4f, 0.4f, 0.4f,
+                -0.5f,  0.5f,0.4f, 0.4f, 0.4f,
 
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f,  0.5f,
+                -0.5f, -0.5f,0.4f, 0.4f, 0.4f,
+                0.5f, -0.5f,0.4f, 0.4f, 0.4f,
+                0.5f,  0.5f,0.4f, 0.4f, 0.4f,
 //triangle fan
-                0,0,
-                -0.45f, -0.45f,
-                0.45f,  -0.45f,
-                0.45f,  0.45f,
-                -0.45f, 0.45f,
-                -0.45f, -0.45f,
+                0,0,1f,   1f,   1f,
+                -0.45f, -0.45f, 0.7f, 0.7f, 0.7f,
+                0.45f,  -0.45f,0.7f, 0.7f, 0.7f,
+                0.45f,  0.45f,0.7f, 0.7f, 0.7f,
+                -0.45f, 0.45f,0.7f, 0.7f, 0.7f,
+                -0.45f, -0.45f,0.7f, 0.7f, 0.7f,
 // Line 1
-                -0.5f, 0f,
-                0.5f, 0f,
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
 // Mallets
-                0f, -0.25f,
-                0f, 0.25f
+                0f, -0.25f, 0f, 0f, 1f,
+                0f,  0.25f, 1f, 0f, 0f,
 
         };
+
         vertexData = ByteBuffer.allocateDirect(tableVertices.length*BYTES_PER_FLOAT)
         .order(ByteOrder.nativeOrder()). asFloatBuffer();
         vertexData.put(tableVertices);
@@ -96,12 +102,16 @@ public class Air_Hockey_Renderer implements GLSurfaceView.Renderer {
         }
         glUseProgram(program);
 
-        uColorLocation = glGetUniformLocation(program, U_COLOR);
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
         aPositionLocation = glGetAttribLocation(program,A_POSITION);
 
         vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation,POSITION_COMPONENT_COUNT,GL_FLOAT,false,0,vertexData);
+        glVertexAttribPointer(aPositionLocation,POSITION_COMPONENT_COUNT,GL_FLOAT,false,STRIDE,vertexData);
         glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation,COLOR_COMPONENT_COUNT,GL_FLOAT,false,STRIDE,vertexData);
+        glEnableVertexAttribArray(aColorLocation);
     }
 
     @Override
@@ -112,15 +122,10 @@ public class Air_Hockey_Renderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
-        glUniform4f(uColorLocation,0.8f,0.8f,0.8f,1.0f);
         glDrawArrays(GL_TRIANGLES,0,6);
-        glUniform4f(uColorLocation,1.0f,1.0f,1.0f,1.0f);
         glDrawArrays(GL_TRIANGLE_FAN,6,6);
-        glUniform4f(uColorLocation,1.0f,0.0f,0.0f,1.0f);
         glDrawArrays(GL_LINES,12,2);
-        glUniform4f(uColorLocation,0.0f,0.0f,1.0f,1.0f);
         glDrawArrays(GL_POINTS,14,1);
-        glUniform4f(uColorLocation,1.0f,0.0f,0.0f,1.0f);
         glDrawArrays(GL_POINTS,15,1);
     }
 }
